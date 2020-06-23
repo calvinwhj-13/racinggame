a483f1e464df16471168085a84d797e9d115b7b8
calvinwhj-13 <64308391+calvinwhj-13@users.noreply.github.com>
2020-06-23 16:22:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var form,player,game;
+var gamestate = 0;
+var playercount;
+var allplayers;
+var car1,car2,car3,car4;
+var cars;
+
+function setup() {
+  createCanvas(displayWidth-20,displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getstate();
+  game.start(); 
+}
+
+function draw() {
+  if(playercount===4) {
+    game.update(1);
+  }
+
+  if(gamestate===1) {
+    clear();
+    game.play();
+  }
+  
+}
